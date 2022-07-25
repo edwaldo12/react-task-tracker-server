4c9081d2dc532f1bd44de3d8e690f17237b1daf4
Edwaldo Utama <53479749+edwaldo12@users.noreply.github.com>
2022-07-25 21:41:49 +0700

    done
    done

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+const port = process.env.PORT || 5000;
+
 server.use(middlewares);
 server.use(router);
-server.listen(process.env.PORT || 5000 , () => {
+server.listen(port , () => {
   console.log("JSON Server is running");
 });
